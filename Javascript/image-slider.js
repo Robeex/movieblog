b43dedc1119661e51eb05fd09cb1fee3b0ef3d31
Robeex <robin.kjellman@outlook.com>
2021-06-07 01:32:11 +0200

    yo

--- Javascript/image-slider.js
+++ Javascript/image-slider.js
@@ -1,14 +1,1 @@
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+var slideIndex=0;function carousel(){var e,l=document.getElementsByClassName("mySlides");for(e=0;e<l.length;e++)l[e].style.display="none";++slideIndex>l.length&&(slideIndex=1),l[slideIndex-1].style.display="block",setTimeout(carousel,2e3)}carousel();
